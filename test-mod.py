93253a34e6c7f4c93cea46ec6cd8ce6b8b973c76
Philip Davis <philipdavis01@gmail.com>
2024-02-02 16:46:05 -0500

    Added trivial test module

---
+++ test-mod.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def query(name, version, lb, ub):
+    print('foo')
+    return(np.ones((2,2)))
+
+if __name__  == "__main__":
+    res = query('abc', 2, (1,2),(3,4))
+    print(res)
